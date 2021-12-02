2f5d52a86784609ad8b0e168f4ceb016d762ca17
cisasteelersfan <cisasteelersfan@gmail.com>
2021-12-01 21:42:01 -0500

    Day 1 complete.

--- day1/solution.go
+++ day1/solution.go
@@ -24,5 +24,19 @@ func main() {
 			c++
 		}
 	}
-	fmt.Println(c)
+	fmt.Println("Part 1:", c)
+
+	c = 0
+	lastThreeTotal := 0
+	for i := 0; i < 3; i++ {
+		lastThreeTotal = lastThreeTotal + n[i]
+	}
+	for i := 3; i < len(n); i++ {
+		prev := lastThreeTotal
+		lastThreeTotal = lastThreeTotal - n[i-3] + n[i]
+		if lastThreeTotal > prev {
+			c++
+		}
+	}
+	fmt.Println("Part 2:", c)
 }
